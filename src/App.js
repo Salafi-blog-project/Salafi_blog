9b5a70a1b8b4ad94bf2f9a0ac3f051e01bd07e89
lukman <abdulrauflukmann9@gmail.com>
2022-07-30 18:47:47 +0100

    app.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Salafi Blog</h1>
+      <p>This is a salafi blog</p>
+    </div>
+  );
+}
+
+export default App;
