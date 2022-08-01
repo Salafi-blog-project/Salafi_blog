8386b69f77e9c632447be19b2eda9dcb62814261
lukman <abdulrauflukmann9@gmail.com>
2022-08-01 15:29:31 +0100

    add styles to the card component

--- src/constants/components/cards/card.js
+++ src/constants/components/cards/card.js
@@ -1,21 +1,23 @@
 import React from "react";
 import styled from "styled-components";
-import { colors } from "../../color ";
+import { colors } from "../../color";
 
 const Card = () => {
   return (
     <CardComponent>
-      <DetailsContainer className="">
-        <div className="flex ">
+      <DetailsContainer className="p-5">
+        <div className="flex justify-between ">
           <div>Design tools</div>
           <div>AUGust 13, 2021 </div>
         </div>
-        <div>
-          10 Hilarious Cartoons That Depict Real-Life Problems of Programmers
-        </div>
-        <div>
-          Redefined the user acquisition and redesigned the onboarding
-          experience, all within 3 working weeks.
+        <div className="gap-y-3">
+          <div className="text-left	">
+            10 Hilarious Cartoons That Depict Real-Life Problems of Programmers
+          </div>
+          <div className="text-left	">
+            Redefined the user acquisition and redesigned the onboarding
+            experience, all within 3 working weeks.
+          </div>
         </div>
       </DetailsContainer>
 
@@ -30,9 +32,11 @@ const CardComponent = styled.div`
   width: 70vw;
   height: 20vh;
   display: flex;
-  padding: 10rem;
+  padding: 5rem;
 `;
 
 const DetailsContainer = styled.div`
   background-color: ${colors.deepblue};
+  width: 70%;
+  height: 200px;
 `;
