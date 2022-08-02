8543508b285255475d6445d172e32ea8cbcbec0c
lukman <abdulrauflukmann9@gmail.com>
2022-08-02 18:10:00 +0100

    passed some props to the card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import Card from "./constants/components/cards/card";
+import image1 from "./assets/room_card.svg";
 
 function App() {
   return (
-    <div className="App">
-      <Card />
+    <div>
+      <Card
+        img1={image1}
+        subtitle="Redefined the user acquisition and redesigned the onboarding experience, all within 3 working weeks."
+        title="10 Hilarious Cartoons That Depict Real-Life Problems of Programmers"
+        date="AUGust 13, 2021"
+        name=" Design tools"
+      />
     </div>
   );
 }
