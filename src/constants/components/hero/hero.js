e184ab4e10a96f1e50bccbe8c7e25f8fbba59eaf
lukman <abdulrauflukmann9@gmail.com>
2022-08-03 14:32:23 +0100

    add some styles to the hero.js file

--- src/constants/components/hero/hero.js
+++ src/constants/components/hero/hero.js
@@ -5,8 +5,24 @@ import { colors } from "../../color";
 const HeroSection = () => {
   return (
     <HeroSectionContainer>
-      <div>1</div>
-      <div>2</div>
+      <div className="flex justify-between p-7">
+        <div className="text-white">Salafi Blog</div>
+        <MiddleContainer className="text-white">
+          <div>Design Tools</div>
+          <div>Daily Updates</div>
+          <div>Tutorials</div>
+          <div>Library</div>
+        </MiddleContainer>
+        <div className="text-white w-22 rounded-md cursor-pointer h-7 px-2 bg-slate-500">
+          Subscribe
+        </div>
+      </div>
+      <div className="flex justify-center items-center flex-col font-bold	text-white ">
+        <div className="text-left	">As-Salaam Alaikum</div>
+        <div className="text-3xl">
+          Insights about my personal and work <br/> life, and the in-betweens
+        </div>
+      </div>
     </HeroSectionContainer>
   );
 };
@@ -15,6 +31,13 @@ export default HeroSection;
 
 const HeroSectionContainer = styled.div`
   background-color: ${colors.deepblue};
-  width: 60vw;
-  height: 20vh;
+  width: 80vw;
+  height: 40vh;
+  border-radius: 0.5rem;
+`;
+
+const MiddleContainer = styled.div`
+  width: 50%;
+  display: flex;
+  justify-content: space-between;
 `;
