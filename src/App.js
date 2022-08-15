df2e42396b773e44d876753a870e7206cc708106
Lukman10a <abdulrauflukman9@gmail.com>
2022-08-15 14:57:09 +0100

    delete some folders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import Card from "./constants/components/cards/card";
-import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import Card from "./components/cards/card";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
-import HeroSection from "./constants/components/hero/hero";
-import FooterSection from "./constants/components/footer/footer";
-import { Room, Dining, Flower } from "../src/constants/components/image";
-import CardDetails from "./constants/pages/carddetails";
+import HeroSection from "./components/hero/hero";
+import FooterSection from "./components/footer/footer";
+import { Room, Dining, Flower } from "../src/components/image";
+import CardDetails from "./pages/carddetails";
 
 function App() {
   const [cards, setCard] = useState([
@@ -40,27 +40,33 @@ function App() {
   ]);
 
   return (
-    <div className="flex justify-center items-center flex-col my-1">
-      <HeroSection />
-      <div>
-        {cards.map((card) => (
-          <Card
-            key={card.id}
-            id={card.id}
-            img={card.img}
-            subtitle={card.subtitle}
-            title={card.title}
-            date={card.date}
-            name={card.name}
-          />
-        ))}
+    <BrowserRouter>
+      <div className="flex justify-center items-center flex-col my-1">
+        <HeroSection />
+        <div>
+          {cards.map((card) => (
+            <Card
+              key={card.id}
+              id={card.id}
+              img={card.img}
+              subtitle={card.subtitle}
+              title={card.title}
+              date={card.date}
+              name={card.name}
+            />
+          ))}
+        </div>
+        <FooterSection
+          subscribe="Subscribe to my blog."
+          content="I post fresh content every week."
+        />
+        <Switch>
+          <Route>
+            <CardDetails />
+          </Route>
+        </Switch>
       </div>
-      <FooterSection
-        subscribe="Subscribe to my blog."
-        content="I post fresh content every week."
-      />
-      <CardDetails/>
-    </div>
+    </BrowserRouter>
   );
 }
 
