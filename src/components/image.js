cb09f5a820e7e5e15a66a341264d35bb0162f8ec
Lukman10a <abdulrauflukman9@gmail.com>
2022-08-16 13:32:06 +0100

    made some changes

--- src/components/image.js
+++ src/components/image.js
@@ -1,3 +1,3 @@
-export { default as Room } from "../../../src/assets/room_card.svg";
-export { default as Dining } from "../../../src/assets/dining_card.svg";
-export { default as Flower } from "../../../src/assets/flower.svg";
+export { default as Room } from "../assets/room_card.svg";
+export { default as Dining } from "../assets/dining_card.svg";
+export { default as Flower } from "../assets/flower.svg";
