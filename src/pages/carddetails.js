0f16062bef055dbf1860b5871f914a70e330dba7
Lukman10a <abdulrauflukman9@gmail.com>
2022-08-23 13:33:55 +0100

    made some changes

--- src/pages/carddetails.js
+++ src/pages/carddetails.js
@@ -1,14 +1,50 @@
 import React from "react";
 
-const CardDetails = ({url}) => {
-    return (  
+const CardDetails = () => {
+  return (
     <div>
-        <img src="../assets/details_image.png" alt=""/>
-        <div></div>
-        <div></div>
-        <div></div>
+      <img src="../assets/detils_image.png" alt="img1" />
+      <div className="p-20 px-48">
+        <p className="py-3">
+          Design comps, layouts, wireframes—will your clients accept that you go
+          about things the facile way? Authorities in our business will tell in
+          no uncertain terms that Lorem Ipsum is that huge, huge no no to
+          forswear forever.
+        </p>
+
+        <p className="py-3">
+          Not so fast, I'd say, there are some redeeming factors in favor of
+          greeking text, as its use is merely the symptom of a worse problem to
+          take into consideration.
+        </p>
+        <p>
+          The toppings you may chose for that TV dinner pizza slice when you
+          forgot to shop for foods, the paint you may slap on your face to
+          impress the new boss is your business. But what about your daily
+          bread?
+        </p>
+      </div>
+      <div>
+        <img src="" alt="img2" />
+      </div>
+      <div className="py-4 px-48">
+        <p>
+          The toppings you may chose for that TV dinner pizza slice when you
+          forgot to shop for foods, the paint you may slap on your face to
+          impress the new boss is your business. But what about your daily
+          bread? Not so fast, I'd say, there are some redeeming factors in favor
+          of greeking text, as its use is merely the symptom of a worse problem
+          to take into consideration.
+        </p>
+        <p>
+          Design comps, layouts, wireframes—will your clients accept that you go
+          about things the facile way? Authorities in our business will tell in
+          no uncertain terms that Lorem Ipsum is that huge, huge no no to
+          forswear forever.
+        </p>
+      </div>
     </div>
-    );
-}
- 
+  );
+};
+
 export default CardDetails;
