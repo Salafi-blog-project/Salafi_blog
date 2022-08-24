ed25d2da29657832f69f0e26477944b3c9516991
Lukman10a <abdulrauflukman9@gmail.com>
2022-08-24 13:05:26 +0100

    add a state to the Ho,e component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,40 +1,12 @@
 import React from 'react';
 import { useState } from "react";
 import Card from "../components/cards/card";
-import { Room, Dining, Flower } from "../components/image";
+import { blogdata } from './blogdata';
+
 
 const HomePage = () => {
 
-    const [cards, setCard] = useState([
-        {
-          id: 1,
-          name: " Design tools",
-          date: "AUGust 13, 2021",
-          img: Room,
-          title:
-            "10 Hilarious Cartoons That Depict Real-Life Problems of Programmers",
-          subtitle:
-            "Redefined the user acquisition and redesigned the onboarding experience, all within 3 working weeks.",
-        },
-        {
-          name: " Design tools",
-          date: "AUGust 13, 2021",
-          img: Dining,
-          title:
-            "10 Hilarious Cartoons That Depict Real-Life Problems of Programmers",
-          subtitle:
-            "Redefined the user acquisition and redesigned the onboarding experience, all within 3 working weeks.",
-        },
-        {
-          name: " Design tools",
-          date: "AUGust 13, 2021",
-          img: Flower,
-          title:
-            "10 Hilarious Cartoons That Depict Real-Life Problems of Programmers",
-          subtitle:
-            "Redefined the user acquisition and redesigned the onboarding experience, all within 3 working weeks.",
-        },
-      ]);
+    const [cards, setCard] = useState(blogdata);
     return (
 <div>
           {cards.map((card) => (
