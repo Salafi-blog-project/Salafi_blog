6ed6ed968742499100ac528d430e9b0cb5e3b9df
Lukman10a <abdulrauflukman9@gmail.com>
2022-09-04 22:16:38 +0100

    made a change to the postlist file

--- src/components/postlist.js
+++ src/components/postlist.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import postlist from "../posts.json"
+import Markdown from 'react-markdown';
 
 const Postlist = () => {
     console.log(postlist)
     return (  
-        <div>postlist</div>
+        <div>
+        <h1>All Posts</h1>
+        {postlist.map((post, i) => {
+            return(
+                <div>
+                    <div>{post.title}</div>
+                    <div>published on {post.date} by {post.author}</div>
+                    <div>{post.content}</div>
+                </div>
+            )
+        })}
+        </div>
     );
 }
  
