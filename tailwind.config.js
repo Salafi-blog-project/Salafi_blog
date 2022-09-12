44c13fb981c07019d08e67925e0291789b8caf61
Lukman10a <abdulrauflukman9@gmail.com>
2022-09-12 15:18:07 +0100

    add some breaking points to tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,5 +13,15 @@ module.exports = {
   theme: {
     extend: {},
   },
+  screens: {
+    'sm': '640px',
+    // => @media (min-width: 640px) { ... }
+
+    'md': '1024px',
+    // => @media (min-width: 1024px) { ... }
+
+    'lg': '1280px',
+    // => @media (min-width: 1280px) { ... }
+  },
   plugins: [],
 };
