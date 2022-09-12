845351a763cfdf767e6a4882e26640ba6b05cc37
Lukman10a <abdulrauflukman9@gmail.com>
2022-09-12 21:37:56 +0100

    made some changes to the home component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,28 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import Card from "../components/cards/card";
-import postlist from "../posts.json"
+import postlist from "../posts.json";
 
 const HomePage = () => {
+  const excerptList = postlist.map((post) => {
+    return post.content.split(" ").slice(0, 10).join(" ");
+  });
 
-    const excerptList = postlist.map((post) => {
-      return post.content.split(" ").slice(0, 10).join(" ")
-  })
+  return (
+    <div>
+      {postlist.map((post, i) => (
+        <Card
+          key={post.id}
+          id={post.id}
+          image={post.image}
+          content={excerptList[i]}
+          title={post.title}
+          date={post.date}
+          author={post.author}
+        />
+      ))}
+    </div>
+  );
+};
 
-    return (
-        <div>
-          {postlist.map((post, i) => (
-            <Card
-            key={post.id}
-              id={post.id}
-              image={post.image}
-              content={excerptList[i]}
-              title={post.title}
-              date={post.date}
-              name={post.name}
-            />
-          ))}
-         </div>
-    );
-}
- 
 export default HomePage;
