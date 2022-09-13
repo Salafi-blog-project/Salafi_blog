c5d03ad1a3cf7dbf9b22bf27a6eb2e1341d83468
Lukman10a <abdulrauflukman9@gmail.com>
2022-09-13 14:41:45 +0100

    made some changes

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { colors } from "../../constants/color";
 import NavBar from "../navbar/navbar";
@@ -7,10 +6,10 @@ import NavBar from "../navbar/navbar";
 const HeroSection = () => {
   return (
     <HeroSectionContainer>
-      <NavBar/>
+      <NavBar />
       <div className="flex justify-center items-center flex-col font-bold	text-white py-5">
-        <div className="text-3xl">
-          Insights about my personal and work <br/> life, and the in-betweens
+        <div className="text-3xl md:flex hidden">
+          Insights about my personal and work <br /> life, and the in-betweens
         </div>
       </div>
     </HeroSectionContainer>
@@ -21,24 +20,21 @@ export default HeroSection;
 
 const HeroSectionContainer = styled.div`
 
+background-color: ${colors.deepblue};
+width: 80vw;
+height: 40vh;
+border-radius: 0.5rem;
+padding: 1.5rem;
 
-  @media (min-width: 320px) {
+  // @media (min-width: 480px) {
+  //   background-color: ${colors.deepblue};
+  //   border-radius: 0.5rem;
+  //   width: 80%;
+  // }
+
+  @media (min-width: 880px) {
   }
 
-  @media (min-width: 640px) {
-  }
-
-  @media (min-width: 1024px) {
-    background-color: ${colors.deepblue};
-    width: 80vw;
-    height: 40vh;
-    border-radius: 0.5rem;
-    padding:1.5rem;
-  }
+  
+  
 `;
-
-// const MiddleContainer = styled.div`
-//   width: 50%;
-//   display: flex;
-//   justify-content: space-between;
-// `;
