35b9c70152733d80759eabcc99b4dd03c27a118e
Lukman10a <abdulrauflukman9@gmail.com>
2022-09-15 13:57:19 +0100

    made some changes

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -7,12 +7,14 @@ const Card = ({ image, title, date, author, content, id }) => {
   return (
     <CardComponent>
       <DetailsContainer className="p-5">
-        <div className="flex justify-between gap-2 w-full">
+        <TitleAndDate className="flex justify-between gap-2 w-full">
+          <div className="bg-blue-100 p-2 px-3 rounded">
           <Link to={`/blog/${id}`} className="font-bold">
             {title}
           </Link>
-          <div className="">{date} </div>
-        </div>
+          </div>
+          <div className="py-2">{date} </div>
+        </TitleAndDate>
         <div>
           <ReactMarkdown children={content} />
           <div>written by: {author} </div>
@@ -34,39 +36,27 @@ const CardComponent = styled.div`
 
   @media (max-width: 480px) {
     flex-direction: column-reverse;
-  }
-
-  // @media (max-width: 880px) {
-  //   border-top: 0.1rem solid #ebf2fe;
-  //   padding:2rem;
-  // }
-
-
-  
+  }  
 `;
 
 const DetailsContainer = styled.div`
-  @media (min-width: 480px) {
-    width: 70%;
-  }
-
-  @media (min-width: 880px) {
-  }
-
-  @media (min-width: 1024px) {
+  @media (max-width: 480px) {
+    width: 100%;
   }
 `;
 
 const ImageContainer = styled.div`
-  @media (min-width: 480px) {
+  @media (max-width: 480px) {
     display: flex;
     justify-content: center;
     align-items: center;
   }
 
-  @media (min-width: 880px) {
+`;
+
+const TitleAndDate = styled.div`
+  @media (max-width: 480px) {
+    padding-bottom: 1rem;
   }
 
-  @media (min-width: 1024px) {
-  }
 `;
