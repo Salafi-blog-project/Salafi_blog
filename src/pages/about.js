ba70b345adb268ca1b1d3df6a47d2a2ff3b38182
Lukman10a <abdulrauflukman9@gmail.com>
2023-05-26 11:44:10 +0100

    add some changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
-import { blogimages } from './blogimages';
+import { blogimages } from "./blogimages";
 // import { Image1, Image2, Image3, Image4 } from "../components/image";
 
 const About = () => {
+  const [images, setImage] = useState(blogimages);
 
-const [images, setImage] = useState(blogimages)
-
-  return <div>
-    {images.map((image) => (
-      <div className="">
-        <img src={image} alt="image"/>
-      </div>
-    ))}
-  </div>;
+  return (
+    <div>
+      {images.map((image) => (
+        <div className="">
+          <img src={image} alt="" />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default About;
